d5aff732be58152d74aeb1b9a70aa467714f008c
Felipe Morales C <fmoralesch@outlook.com>
2024-06-24 18:41:50 -0400

    refactor: fixing publish method call and others refactors

--- servicio-1/service.py
+++ servicio-1/service.py
@@ -8,25 +8,36 @@ def connect_mqtt() -> mqtt_client.Client:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-    client: mqtt_client.Client = mqtt_client.Client()
+    client = mqtt_client.Client()
     client.on_connect = on_connect
     client.connect(const.BROKER, const.PORT)
     return client
 
 def publish(client: mqtt_client.Client) -> None:
     while True:
-        msg: str = f"message"
-        result: mqtt_client.MQTTMessageInfo = client.publish(const.TOPIC, msg)
+        msg: str = """
+{
+    "time": datetime,
+    "value": random(0, 1000),
+    "version": random(1, 2) 
+}
+"""
+        result: mqtt_client.MQTTMessageInfo = client.publish(
+            topic=const.TOPIC, 
+            payload=msg
+            )
         status: int = result[0]
         if status == 0:
             print(f"Successful send {msg} to topic {const.TOPIC}")
+        else:
+            print(f"Error sending {msg} to topic {const.TOPIC}")
         sleep(const.TIME)
 
 
 def main():
     client = connect_mqtt()
     client.loop_start()
-    client.publish(client)
+    publish(client)
     client.loop_stop()
 
 if __name__ == "__main__":
