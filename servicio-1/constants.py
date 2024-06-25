a099c5f58deecc6afbbcafc36808a759e2df8e55
Felipe Morales C <fmoralesch@outlook.com>
2024-06-25 10:54:39 -0400

    chore: adding MESSAGE_TEMPLATE with message structure

--- servicio-1/constants.py
+++ servicio-1/constants.py
@@ -3,3 +3,8 @@ TOPIC: str = "challenge/dispositivo/rx"
 BROKER: str = "challenges-ta-mosquitto-1"
 PORT: int = 1883
 MAX_PUBLICATION_TIME: int = 5
+MESSAGE_TEMPLATE = """{{
+"time": "{time}",
+"value": {value},"
+"version: {version}"
+}}"""
