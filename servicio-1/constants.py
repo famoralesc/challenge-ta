1d3c9844bc21213070b00091ab4a401732d2309b
Felipe Morales C <fmoralesch@outlook.com>
2024-06-24 23:20:09 -0400

    feat: implementing message and set maximun publication times

--- servicio-1/constants.py
+++ servicio-1/constants.py
@@ -2,3 +2,4 @@ TIME: int = 60
 TOPIC: str = "challenge/dispositivo/rx"
 BROKER: str = "challenges-ta-mosquitto-1"
 PORT: int = 1883
+MAX_PUBLICATION_TIME: int = 5
--- servicio-1/service.py
+++ servicio-1/service.py
@@ -1,44 +1,62 @@
-from paho.mqtt import client as mqtt_client 
-import constant as const
+import random
 from time import sleep
+from datetime import datetime
+from paho.mqtt import client as mqtt_client
+import constants as const
+
+client_id = f"python-mqtt-{random.randint(0, 1000)}"
+
 
 def connect_mqtt() -> mqtt_client.Client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
+    print("Connecting...")
+
+    def on_connect(client, userdata, flags, reason_code):
+        if reason_code == 0:
             print("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
-    client = mqtt_client.Client()
+            print("Failed to connect, return code %d\n", reason_code)
+
+    client = mqtt_client.Client(client_id=client_id)
     client.on_connect = on_connect
     client.connect(const.BROKER, const.PORT)
     return client
 
+
 def publish(client: mqtt_client.Client) -> None:
+    times = 0
     while True:
-        msg: str = """
-{
-    "time": datetime,
-    "value": random(0, 1000),
-    "version": random(1, 2) 
-}
-"""
+        times += 1
+        now = datetime.now()
+        date_time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        msg = (
+            "{\n"
+            f"time: {date_time_str}\n,"
+            f"value: {random.randint(0,1000)}\n,"
+            f"version: {random.randint(0,2)}\n"
+            "}"
+        )
+
         result: mqtt_client.MQTTMessageInfo = client.publish(
-            topic=const.TOPIC, 
-            payload=msg
-            )
+            topic=const.TOPIC, payload=msg
+        )
         status: int = result[0]
         if status == 0:
             print(f"Successful send {msg} to topic {const.TOPIC}")
         else:
             print(f"Error sending {msg} to topic {const.TOPIC}")
         sleep(const.TIME)
+        if times == const.MAX_PUBLICATION_TIME:
+            break
 
 
-def main():
+def main() -> None:
+    print("Initializing...")
     client = connect_mqtt()
     client.loop_start()
     publish(client)
     client.loop_stop()
+
 
 if __name__ == "__main__":
     main()
