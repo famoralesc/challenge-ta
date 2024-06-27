99c13773207fab821e19532efa8af493efb7fdbe
Felipe Morales C <fmoralesch@outlook.com>
2024-06-27 15:24:01 -0400

    feat: adding process and search feature from home page

--- servicio-3/challenge/challenge_app/static/js/datatables-simple-demo.js
+++ servicio-3/challenge/challenge_app/static/js/datatables-simple-demo.js
@@ -1,9 +1,123 @@
 window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
+    
+    // on load
+    const versionInput = document.getElementById('versionForSearch');
+    
+    const datatablesSimple = document.getElementById('datatablesAlerts');
+    console.log("Loading data")
+    const dataTable = new simpleDatatables.DataTable(datatablesSimple);
+    fetch('/challenge/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ version: versionInput.value }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        let finalData = {
+            data: []
+        }
+        data.forEach(d => {
+            finalData.data.push(
+                [
+                    d.datetime, 
+                    d.value, 
+                    d.version,
+                    d.type, 
+                    d.sended
+                ]
+            )
+        });
+        console.log('Success:', finalData);
+        dataTable.insert(finalData);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
+    // on searchBtn is clicked
+
+    const btnSearch = document.getElementById("btnSearch");
+    btnSearch.addEventListener('click', () =>{
+        const typeInput = document.getElementById("type")
+        const sendedInput = document.getElementById("sended")
+        let data = { version: versionInput.value };
+
+        if (sendedInput.value !== ''){
+            sendedValue = true ? sendedInput.value === "true" : false;
+            data["sended"] = sendedValue;
+        }
+        if (typeInput.value !== ''){
+            data["type"] = typeInput.value;
+        }
+        
+        fetch('/challenge/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            let finalData = {
+                data: []
+            }
+            dataTable.destroy();
+            dataTable.init();
+            data.forEach(d => {
+                finalData.data.push(
+                    [
+                        d.datetime, 
+                        d.value, 
+                        d.version,
+                        d.type, 
+                        d.sended
+                    ]
+                )
+            });
+            dataTable.insert(finalData);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    })
+    
+    // on btnProcess clicked
+    const btnProcess = document.getElementById("btnProcess");
+    btnProcess.addEventListener('click', () =>{
+        const timeInput = document.getElementById("timeInput")
+    
+        let data = { version: versionInput.value };
+
+        if (timeInput.value !== ''){
+            data["timeSearch"] = timeInput.value;
+        }
+        else{
+            alert("El campo Time es obligatorio para procesar la data");
+            return;
+        }
+        
+        fetch('/challenge/process', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.status != 'ok'){
+                alert(data.error);
+                return
+            }
+            alert("Procesado ok");
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    })
+    
 });
