fcef64b88e2c42f2f0471534c58e437ba70e9140
Felipe Morales C <fmoralesch@outlook.com>
2024-06-26 23:29:36 -0400

    chore: adding constant for alerts level

--- servicio-3/challenge/challenge_app/constants.py
+++ servicio-3/challenge/challenge_app/constants.py
@@ -3,9 +3,14 @@ INFLUX_URL: str = "http://challenges-ta-influx-1:8086"
 INFLUX_ORG: str = "tecnoandina"
 INFLUX_BUCKET: str = "system"
 INFLUX_MEASUREMENT: str = "dispositivos"
-INFLUX_TOKEN: str = "FUSSVXb2_qCnc3zBZglzoFn4QO_KxAto3R7mjCGN_jhDk-lhpo6kEkg0K-2e3vMlYO4hWa7r7Ou789HwoiR5vQ=="
+INFLUX_TOKEN: str = (
+    "FUSSVXb2_qCnc3zBZglzoFn4QO_KxAto3R7mjCGN_jhDk-lhpo6kEkg0K-2e3vMlYO4hWa7r7Ou789HwoiR5vQ=="
+)
 
 
 # API CONSTANTS
 VERSION: str = "version"
 TIME_SEARCH: str = "timeSearch"
+BAJA: str = "BAJA"
+MEDIA: str = "MEDIA"
+ALTA: str = "ALTA"
