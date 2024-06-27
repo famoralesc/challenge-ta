499b6a5748bd984077d03b8f1a9aa485fa357acd
Felipe Morales C <fmoralesch@outlook.com>
2024-06-27 01:22:13 -0400

    chore: setting up mysql db and ignore sqlite from repo

--- servicio-3/challenge/challenge/settings.py
+++ servicio-3/challenge/challenge/settings.py
@@ -74,9 +74,16 @@ WSGI_APPLICATION = "challenge.wsgi.application"
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tecnoandina',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'challenges-ta-mysql-1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8', 
+        },
     }
 }
 
