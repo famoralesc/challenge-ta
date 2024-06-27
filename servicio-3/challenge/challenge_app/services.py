b43abaa6c152e1116eac9969323ace34f1bdb1fe
Felipe Morales C <fmoralesch@outlook.com>
2024-06-26 23:30:59 -0400

    feat: implement service layer for process api

--- servicio-3/challenge/challenge_app/services.py
+++ servicio-3/challenge/challenge_app/services.py
@@ -1,31 +1,62 @@
 from challenge_app.repository import influx as influx_repo
-from challenge_app.constants import VERSION, TIME_SEARCH
+from challenge_app.constants import BAJA, MEDIA, ALTA
 
-def get_data_from_influx(version: int, time_search: str) -> dict:
+
+def get_data_from_influx(version: int, time_search: str) -> list[dict]:
     client = influx_repo.get_client()
-    raw_data = influx_repo.get(client, version, time_search)
-    # PROCESS RAW_DATA 
-    return {}
+    query = influx_repo.get_query(version, time_search)
+    return influx_repo.get(client, query)
+
 
 def validate_time_search_format(time_search: str) -> bool:
-    rules = [
-        "m" in time_search,
-        "h" in time_search,
-        "d" in time_search
-    ]
+    rules = ["m" in time_search, "h" in time_search, "d" in time_search]
     return any(rules)
 
 
-def process(parameters: dict) -> dict:
-    # VALIDATE PARAMETERS
-    version = parameters[VERSION]
-    time_search = parameters[TIME_SEARCH]
+def get_alert_type(version: int, value: int) -> str:
+    if version == 1:
+        match value:
+            case v if v > 200 and v < 500:
+                return BAJA
+            case v if v > 500 and v < 800:
+                return MEDIA
+            case v if v > 800:
+                return ALTA
+            case _:
+                raise ValueError(f"No se puede determinar el type a partir del value {value}")
+    if version == 2:
+        match value:
+            case v if v < 200:
+                return BAJA
+            case v if v < 500 and v > 200:
+                return MEDIA
+            case v if v < 800 and v > 500:
+                return ALTA
+            case _:
+                raise ValueError(f"No se puede determinar el type a partir del value {value}")
 
+
+def get_data_to_persist(version: int, data: list[dict]) -> list[dict]:
+    if not data:
+        return []
+    
+    response: list[dict]= []
+    
+    for record in data:
+        alert_type = get_alert_type(version, record['value'])
+
+
+def process(version: int, time_search: str) -> None:
+    # VALIDATE PARAMETERS
     if not validate_time_search_format(time_search):
         raise ValueError("No se pudo procesar los párametros")
 
+    if not time_search.startswith("-"):
+        time_search = f"-{time_search}"
+
     # GET DATA FROM INFLUX
     influx_data = get_data_from_influx(version, time_search)    
-    # INSERT DATA TO MYSQL
     
-    return {}
+    # INSERT DATA TO MYSQL
+    data = get_data_to_persist(version, influx_data)
+    
